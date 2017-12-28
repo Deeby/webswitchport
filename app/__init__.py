71b068db23ea3fa5c1f1cf0743853fa417400fbd
Konishev Dmitry <anonslou@gmail.com>
2017-12-28 11:51:48 +0300

    secret_key

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,25 @@
 from flask import Flask
 from flask_session import Session
 from werkzeug.contrib.fixers import ProxyFix
-import random
-import string
+import os
+
+app = Flask(__name__)
 
 # head -c 24 /dev/urandom > secret_key
-# filename = 'secret_key'
-# filename = os.path.join(app.instance_path.replace('instance', 'app'), filename)
-# app.config['SECRET_KEY'] = open(filename, 'rb').read()
-app = Flask(__name__)
-# для gunicorn --bind 0.0.0.0:8000 --workers 3 run:app
+filename = 'secret_key'
+filename = os.path.join(app.instance_path.replace('instance', 'app'), filename)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config['SECRET_KEY'] =''.join(random.SystemRandom().choice(string.digits + string.ascii_letters) for _ in range(30))
+app.config['SECRET_KEY'] = open(filename, 'rb').read()
+# app.config['SECRET_KEY'] = ''.join(random.SystemRandom().choice(string.digits + string.ascii_letters) for _ in range(30))
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = 1800
 sess = Session()
 sess.init_app(app)
 
 
-# @app.before_request
-# def make_session_permanent():
-#     sess.permanent = True
+@app.before_request
+def make_session_permanent():
+    sess.permanent = True
 
 
 from app import views
